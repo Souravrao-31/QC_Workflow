6ac378e4e88d7ef38fe5078164fd1eb37c615c8c
Souravrao-31 <souravrao3110@gmail.com>
2025-12-15 15:12:44 +0530

    Server: password column added

--- server/app/models/user.py
+++ server/app/models/user.py
@@ -14,3 +14,4 @@ class User(Base):
     name = Column(String, nullable=False)
     role = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    password_hash = Column(String, nullable=False)
