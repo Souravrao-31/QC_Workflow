33d4ae136de552d2ec0acf7804ca9c7ec61f2477
Souravrao-31 <souravrao3110@gmail.com>
2025-12-17 02:32:41 +0530

    Server: security layer added

--- server/app/core/dependencies.py
+++ server/app/core/dependencies.py
@@ -2,9 +2,11 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 from uuid import UUID
-
+from sqlalchemy.orm import Session
 from app.core.security import SECRET_KEY, ALGORITHM
-
+from app.core.database import get_db
+from app.core.security import decode_access_token
+from app.models.user import User
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
@@ -14,17 +16,25 @@ class CurrentUser:
         self.role = role
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)) -> CurrentUser:
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id = payload.get("sub")
-        role = payload.get("role")
-        if not user_id or not role:
-            raise JWTError()
-    except JWTError:
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db),
+) -> User:
+ 
+    payload = decode_access_token(token)
+
+    if not payload:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid token",
+            detail="Invalid or expired token",
         )
 
-    return CurrentUser(UUID(user_id), role)
+    user = db.query(User).filter(User.id == payload.sub).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found",
+        )
+
+    return user
--- server/app/core/security.py
+++ server/app/core/security.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.context import CryptContext
+from app.core.config import settings
 
 SECRET_KEY = ""
 ALGORITHM = "HS256"
@@ -24,3 +25,14 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     )
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM],
+        )
+        return payload
+    except JWTError:
+        return None
