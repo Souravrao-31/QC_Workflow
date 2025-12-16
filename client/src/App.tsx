e6628246e65e70112f6fe4a90bb90ed5c98b1399
Souravrao-31 <souravrao3110@gmail.com>
2025-12-17 02:07:26 +0530

    Client: route protecting added -> forced routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./auth/LoginPage";
 import DashboardPage from "./dashboard/DashboardPage";
+import RequireAuth from "./auth/RequireAuth";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <RequireAuth>
+              <DashboardPage />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
